2b94a1dcdb416a371ddf288165c3ad90846ff233
Michael Manlulu <michaeldmanlulu@gmail.com>
2026-07-04 05:31:36 +0800

    Update workspace pages

--- app/(main)/workspace/page.tsx
+++ app/(main)/workspace/page.tsx
@@ -1,5 +1,14 @@
+import { Suspense } from 'react';
 import WorkspaceShell from '@/app/(main)/workspace/_components/WorkspaceShell';
+import { PaymentSuccessModal } from '@/app/(main)/workspace/_components/PaymentSuccessModal';
 
 export default function Workspace() {
-  return <WorkspaceShell />;
+  return (
+    <>
+      <WorkspaceShell />
+      <Suspense>
+        <PaymentSuccessModal />
+      </Suspense>
+    </>
+  );
 }
